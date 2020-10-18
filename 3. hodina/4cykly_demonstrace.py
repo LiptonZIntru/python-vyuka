f2a8cd9a7da08699456e220199b405dfc7b04392
LiptonZIntru <vili2002@centrum.cz>
2020-10-18 16:08:35 +0200

    3. hodina

---
+++ 3. hodina/4cykly_demonstrace.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    print("ahoj" + str(i))
+
+
+
+i = 0
+
+while i < 10:
+    i += 1
+    print("ahoj" + str(i))
--- 3. hodina/5hra.py
+++ 3. hodina/5hra.py
@@ -1,3 +1,12 @@
+import random
 
+hadane_cislo = random.randint(1, 10)
 
-hadane_cislo =
+a = int(input("Zadej cislo: "))
+while a != hadane_cislo:
+    if a > hadane_cislo:
+        print("Zadej mensi")
+    else:
+        print("Zadej vetsi")
+    a = int(input("Zadej cislo: "))
+print("Vyhral jsi")
